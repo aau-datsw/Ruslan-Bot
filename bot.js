9dd2451dafea27eda7965351435252203208fe85
Christian Hurley <33719181+HaxorHurley@users.noreply.github.com>
2019-09-12 12:00:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,22 @@
 const Discord = require('discord.js');
+const config = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(auth.token);
+client.login(config.token);
 
-if (message.content === '!ping')  {
-    message.channel.send('pong.');
-}
+client.on('message', message => {
+	if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+	
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'bepp') {
+		message.channel.send('Boop.');
+	}
+});
