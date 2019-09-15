f53e633d93cbd47aaee8061f18c319030f59581f
Mads Smed <mthoma18@student.aau.dk>
2019-09-15 13:31:56 +0200

    Testing basic welcome message

--- bot.js
+++ bot.js
@@ -8,6 +8,17 @@ client.once('ready', () => {
 
 client.login(config.token);
 
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+	// Send the message to a designated channel on a server:
+	const channel = member.guild.channels.find(ch => ch.name === 'bot-testing');
+	console.log("New member!");
+	// Do nothing if the channel wasn't found on this server
+	if (!channel) return;
+	// Send the message, mentioning the member
+	channel.send(`Welcome to the RUSLAN server, ${member}`);
+});
+
 client.on('message', message => {
 	if(!message.content.startsWith(config.prefix) || message.author.bot) return;
 	
@@ -16,8 +27,9 @@ client.on('message', message => {
 	
 	if (command === 'ping') {
 		message.channel.send('Pong.');
-		console.log('Sent a pong');
-	} else if (command === 'bepp') {
+		console.log('Sent a pong!');
+	} else if (command === 'beep') {
 		message.channel.send('Boop.');
+		console.log('send a boop!');
 	}
 });
