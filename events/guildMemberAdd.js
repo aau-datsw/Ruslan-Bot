2a6c56b36f7a72ce8b1d578efb7acfac8dcb558f
888aaen <aaspringborg@gmail.com>
2019-09-17 21:21:11 +0200

    command

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,23 +1,15 @@
+const welcome = require('../commands/welcome');
+
 module.exports = (client, member) => {
-    const welcomeRole = member.guild.roles.find("name", "New user");
+    const welcomeRole = member.guild.roles.find(r => r.name === "New user");
     const channel = member.guild.channels.find(ch => ch.name === 'welcome');
-
-    if (!channel) return;
-
     try {
-        // Give a role to a member
         member.addRole(welcomeRole)
-            .then(channel.send(`Welcome to the RUSLAN server, ${member}`));
+            .then(channel.send(welcome(member))
+            );
     } catch (error) {
         console.log(error);
     }
-
-
-
-
-
-
-
-
-
+    channel.send("efter catch");
+    
 }
--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,17 @@
-module.exports = client => {
+const {client, RichEmbed} = require('discord.js');
+
+module.exports = (client, richEmbed) => {
     console.log(client.user.tag + ' is ready!');
+    const channel = client.channels.find(ch => ch.name === 'welcome');
+    
+    /* 
+    var welcome = new RichEmbed()
+     .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    channel.send(welcome);
+    */
+    
 }
