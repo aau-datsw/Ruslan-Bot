ca9d8cbd0293f43172fa36e69794b48b85d305ff
Mads Smed <mthoma18@student.aau.dk>
2019-09-17 17:48:35 +0200

    Added permission change for new users

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,23 @@
 module.exports = (client, member) => {
-    const channel = member.guild.channels.find(ch => ch.name === 'bot-testing');
-    //console.log(`New member: ${member.name}`);
+    const welcomeRole = member.guild.roles.find("name", "New user");
+    const channel = member.guild.channels.find(ch => ch.name === 'welcome');
+
     if (!channel) return;
 
-    channel.send(`Welcome to the RUSLAN server, ${member}`);
+    try {
+        // Give a role to a member
+        member.addRole(welcomeRole)
+            .then(channel.send(`Welcome to the RUSLAN server, ${member}`));
+    } catch (error) {
+        console.log(error);
+    }
+
+
+
+
+
+
+
+
+
 }
