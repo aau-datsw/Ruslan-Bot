56ecdc6454c39f593a014c2c52ddb0529914651a
888aaen <aaspringborg@gmail.com>
2019-09-24 16:02:52 +0200

    switch statement

--- events/message.js
+++ events/message.js
@@ -2,35 +2,42 @@ module.exports = (client, message) => {
     const config = require('./../config.json');
 
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+    
+    if (message.channel == client.channels.find(ch => ch.name === 'welcome'))
+    {
+        welcomeCommands(client, message);
+    }
 
+}
+
+function welcomeCommands(client, message) {
     const args = message.content.slice(1).split(/ +/);
     const command = args.shift().toLowerCase();
 
     const testChannel = client.channels.find(ch => ch.name === 'bot-testing');
-    const welcomeChannel = client.channels.find(ch => ch.name === 'welcome');
     const ruslingRole = message.member.guild.roles.find(role => { return role.name === "Rusling" });
 
-    if (command === 'ping') {
-        testChannel.send('Pong.');
-        console.log('Sent a pong!');
-    } else if (command === 'beep') {
-        testChannel.send('Boop.');
-        console.log('send a boop!');
-    } else if (command === 'cs') {
-        testChannel.send("CS MASTER!");
-        console.log("a dj");
-    } else if (command === 'rusling') {
-        if (message.member.highestRole.comparePositionTo(ruslingRole) > 0) {
+    switch (command) {
+        case 'ping':
+            testChannel.send('Pong.');
+            console.log('Sent a pong!');
+            break;
+        case 'rusling':
+            if (message.member.highestRole.comparePositionTo(ruslingRole) > 0) {
+                purgeWelcomeChannel(client);
+                return;
+            }
+            // Give rusling role to a member
+            message.member.addRole(ruslingRole)
+                .then(console.log(`${message.member}` + " is rusling"))
+                .catch(error => console.log(error));
             purgeWelcomeChannel(client);
-            return;
-        }
-        // Give rusling role to a member
-        message.member.addRole(ruslingRole)
-            .then(console.log(`${message.member}` + " is rusling"))
-            .catch(error => console.log(error));
-        purgeWelcomeChannel(client);
-    } else if (command === 'tutor') {
-        testChannel.send(message.member + " wants to be tutor!");
+            break;
+        case 'tutor' :
+            testChannel.send(message.member + " wants to be tutor!");
+            break;
+        default:
+            break;
     }
 }
 
