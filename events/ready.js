cd08bcc8d04299d39eec0cea4785c22e15d72484
888aaen <aaspringborg@gmail.com>
2019-09-29 11:44:14 +0200

    welcome richEmbed

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,35 @@
+const Discord = require('discord.js');
+
+
 module.exports = (client) => {
     console.log('The RUSLAN bot is ready!');
+
+    const channel = client.channels.find(ch => ch.name === 'bot-testing'); // change to env file id
+    try {
+        channel.fetchMessages()
+            .then(messages => {
+                const botMsg = messages.filter(fetchedMsg => fetchedMsg.author.id === '621641289183789058');
+                channel.bulkDelete(botMsg);
+            })
+            .catch(console.error);
+    } catch (err) {
+        console.error(err);
+    }
+    
+    
+    const exampleEmbed = new Discord.RichEmbed()
+        .setColor('#ffc800')
+        .setTitle('Velkommen til ruslan')
+        .setURL('https://discord.js.org/') // Link til info om lan
+        .setAuthor('Tutorene', 'https://i.imgur.com/PK1WFTJ.jpg', 'http://sw.rusling.dk')
+        .setDescription('Der her er discorden til RUSlan 2019')
+        .setThumbnail('https://i.imgur.com/rHhIWXe.jpg')
+        .addBlankField()
+        .addField('Rusling', 'skriv "!rusling" for at komme ind på serveren', true)
+        .addField('Tutor', 'kig jeres facebook gruppe for at få adgang', true)
+        .setImage('https://i.imgur.com/p5A0aWR.png')
+        .setTimestamp()
+        .setFooter('Ruslan-bot\'en', 'https://i.imgur.com/piKmQwn.jpg');
+    console.log(exampleEmbed);
+    channel.send(exampleEmbed);
 }
