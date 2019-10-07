7c01fac133b08610ddb714121b5d67c74a93a297
Frederik Spang <frederik@progras.dk>
2019-10-07 16:12:36 +0200

    Fix knikname

--- commands/nickname.js
+++ commands/nickname.js
@@ -1,18 +1,12 @@
 module.exports = (message, args) => {
     try {
         if (args) {
-            name = '';
-            args.forEach(element => {
-                name += element
-                
-            });
-            message.member.setNickname(name);
+            message.member.setNickname(args.join(' '));
         } else {
             message.reply("Provide a name in the command");
         }
-        
+
     } catch (error) {
         console.log(error);
     }
-    
 }
