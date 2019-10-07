4416c3652b6ef016b075875443bd3ab479c2f421
888aaen <aaspringborg@gmail.com>
2019-10-07 15:08:12 +0200

    nickname fix

--- commands/nickname.js
+++ commands/nickname.js
@@ -1,7 +1,12 @@
 module.exports = (message, args) => {
     try {
-        if (args[0]) {
-            message.member.setNickname(args[0]);
+        if (args) {
+            name = '';
+            args.forEach(element => {
+                name += element
+                
+            });
+            message.member.setNickname(name);
         } else {
             message.reply("Provide a name in the command");
         }
