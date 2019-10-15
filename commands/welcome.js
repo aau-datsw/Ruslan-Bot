4e8b13fd9e49fd43ca56c290bfe7f068e8a89b60
888aaen <aaspringborg@gmail.com>
2019-10-15 17:24:41 +0200

    welcome updated

--- commands/welcome.js
+++ commands/welcome.js
@@ -23,10 +23,11 @@ module.exports = (client) => {
         .setDescription('Welcome to the discord server for RUSlan 2019')
         .setThumbnail('https://i.imgur.com/rHhIWXe.jpg')
         .addField('Rules', 'Pls follow the rules -> <#628251600078766111>')
-        .addField('Rusling', '1. Change your nickname to your real name, before joining the server "!nickname YourName"' +
+        .addField('Rusling', '1. Change your nickname to your real name, before joining the server "!nickname Your Real Name Here"' +
             '\n2. Check the ruleset, by clicking the blue link above' +
             '\n3. Type "!rusling" to accept the rules, and get acess to the server' + 
-            '\n   If your confused, type "!help for a list of commands"')
+            '\n   If your confused, type "!help" for a list of commands')
+        .addField('Other type of studnet', '\tSame as rusling, just type "!other" instead', true)
         .addField('Tutor', '\tCheck with your other tutors, to get information on how to join', true)
         .setTimestamp()
         .setFooter('Ruslan-bot\'en', 'https://i.imgur.com/piKmQwn.jpg');
