34b6668a3f029813deaf4905e96f41d3f96b70ba
Anders Aaen Springborg <aaspringborg@gmail.com>
2019-10-19 00:02:44 +0200

    no tutor support fix

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,7 +3,7 @@ module.exports = (client, oldMember, newMember) => {
     const tutorRole = newMember.guild.roles.find(rl => rl.name === 'Ruslan Planlægger');
     const tutorChat = client.channels.find(ch => ch.name === 'tutor-chat');
     console.log(newMember.highestRole);
-    if (newMember.voiceChannel === supportChannel && newMember.highestRole.comparePositionTo(tutorRole)){
+    if (newMember.voiceChannel === supportChannel && (newMember.highestRole.comparePositionTo(tutorRole) < 0)){
         newMember.send(`The tutors have been notified, and are on thier way to the support channel`);
         tutorChat.send(`${tutorRole} ${newMember} needs help. Go to <#${supportChannel.id}>`);
     }
