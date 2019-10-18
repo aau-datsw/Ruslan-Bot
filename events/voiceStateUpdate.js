5799a4baa8f5986fb179e4c5002f31026cd919af
888aaen <aaspringborg@gmail.com>
2019-10-18 10:53:36 +0200

    fix line

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,8 +2,8 @@ module.exports = (client, oldMember, newMember) => {
     const supportChannel = client.channels.find(ch => ch.name === 'Join if you need help');
     const tutorRole = newMember.guild.roles.find(rl => rl.name === 'Ruslan Planlægger');
     const tutorChat = client.channels.find(ch => ch.name === 'tutor-chat');
-
-    if (newMember.voiceChannel === supportChannel && newMember.highestRole < tutorRole){
+    console.log(newMember.highestRole);
+    if (newMember.voiceChannel === supportChannel && newMember.highestRole.comparePositionTo(tutorRole)){
         newMember.send(`The tutors have been notified, and are on thier way to the support channel`);
         tutorChat.send(`${tutorRole} ${newMember} needs help. Go to <#${supportChannel.id}>`);
     }
