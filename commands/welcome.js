318de8dae50fac082f735f67e389773635f89e93
Zaph-x <mikkel@luceartfoto.dk>
2020-09-15 16:48:33 +0200

    Updated to v 12.x and fixed security vulns

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,8 +1,9 @@
 
 const Discord = require('discord.js');
+const config = require('../config.json')
 
 module.exports = (client) => {
-    const channel = client.channels.find(ch => ch.name === 'welcome'); // change to env file id
+    const channel = client.channels.cache.find(ch => ch.name === 'welcome'); // change to env file id
         try {
             channel.fetchMessages()
                 .then(messages => {
@@ -15,14 +16,14 @@ module.exports = (client) => {
         }
 
 
-    const exampleEmbed = new Discord.RichEmbed()
+    const exampleEmbed = new Discord.MessageEmbed()
         .setColor('#ffc800')
         .setTitle('Welcome to RUSlan')
         .setURL('https://docs.google.com/document/d/1M-PhUkAVviJaL6sqBXgLqwL_9f8t5TKqk7buvLpsYMk/edit?usp=sharing') // Rules document
         .setAuthor('Tutorene', 'https://i.imgur.com/PK1WFTJ.jpg', 'http://sw.rusling.dk')
         .setDescription('Welcome to the discord server for RUSlan 2019')
         .setThumbnail('https://i.imgur.com/rHhIWXe.jpg')
-        .addField('Rules', 'Pls follow the rules -> <#628251600078766111>')
+        .addField(`Rules', 'Pls follow the rules -> <#${config.rules_channel}>`)
         .addField('Rusling', '1. Change your nickname to your real name, before joining the server "!nickname Your Real Name Here"' +
             '\n2. Check the ruleset, by clicking the blue link above' +
             '\n3. Type "!rusling" to accept the rules, and get acess to the server' + 
