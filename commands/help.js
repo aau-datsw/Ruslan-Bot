d08342a3ab2ee2ad83ad6fb938e90cd5016b7ee6
cNorspang <casper@norspang.dk>
2020-09-15 21:27:03 +0200

    made !help work, in v12

--- commands/help.js
+++ commands/help.js
@@ -1,29 +1,31 @@
 const Discord = require('discord.js');
+//const { config } = require('dotenv');
+const config = require('../config.json');
 
 module.exports = message => {
-    const adminRole = message.member.guild.roles.find(r => r.name === "Admin");
-    const plannerRole = message.member.guild.roles.find(r => r.name === "Ruslan Planlægger");
-    const ruslingRole = message.member.guild.roles.find(r => r.name === "Rusling");
+    const adminRole = message.member.guild.roles.cache.find(r => r.id === config.admin_role_id);
+    const plannerRole = message.member.guild.roles.cache.find(r => r.id === config.planner_role_id);
+    const ruslingRole = message.member.guild.roles.cache.find(r => r.id === config.pleb_role_id);
 
-    const helpEmbed = new Discord.RichEmbed()
+    const helpEmbed = new Discord.MessageEmbed()
         .setColor('#ffc800')
         .setTitle("**Commands**")
-        .setDescription(`These are the different commands to use for **${message.member.highestRole.name}**:`);
+        .setDescription(`These are the different commands to use for **${message.member.roles.highest.name}**:`);
 
 
         
-    if (message.member.highestRole.comparePositionTo(adminRole) >= 0) {
+    if (message.member.roles.highest.comparePositionTo(adminRole) >= 0) {
         newCommand("purgebot", "purge channel for messages by bot");
         newCommand("purgeme", "purge channel for messages by yourself");
         newCommand("quickpurge", "purge channel for messages by everyone");
     }
-    if (message.member.highestRole.comparePositionTo(plannerRole) >= 0) {
+    if (message.member.roles.highest.comparePositionTo(plannerRole) >= 0) {
         newCommand("setup (tournament)", "!setup -commandName -title -url -description -responsable");
         newCommand("update (tournament)", "!update -commandName -title -url -description -responsable");
         newCommand("maketutor", "add tutor role to @mention1 @mention2...");
 
     }
-    if (message.member.highestRole.comparePositionTo(ruslingRole) >= 0) {
+    if (message.member.roles.highest.comparePositionTo(ruslingRole) >= 0) {
         newCommand("support", "if you need live support");
         newCommand("tournaments", "list all tournament commands");
     } else {
