d2f5fb60b509ec4abed9ac30efb3dd1a5aed21dc
MJHC <magnus@bonusfamilien.dk>
2021-10-17 21:59:38 +0200

    Fixed some bugs in enter and pull

--- src/commands/enter.js
+++ src/commands/enter.js
@@ -14,35 +14,39 @@ module.exports = {
                 .setRequired(true)
         }),
     async execute(interaction){
-        const giveawayChannel = interaction.client.channels.cache.get(config.giveawayChannel);
-        const giveaway = interaction.options.getString('giveaway');
-        let json;
+        try{
+            const giveawayChannel = interaction.client.channels.cache.get(config.giveawayChannel);
+            const giveaway = interaction.options.getString('giveaway');
+            let json;
 
-        try{json = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../draws.json')));} 
-        catch(e){console.log}
+            json = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../draws.json')));
 
-        if(interaction.member.roles.highest.comparePositionTo(config.rusling_role_id) > 0){
-            await interaction.reply({content:'Du skal være rusling for at kunne deltage!', ephemeral: true})
-            return;
-        }
+            if(interaction.member.roles.highest.comparePositionTo(config.rusling_role_id) > 0){
+                await interaction.reply({content:'Du skal være rusling for at kunne deltage!', ephemeral: true});
+                return;
+            }
 
-        const draw = json.draws.find(draw => draw.name.toLowerCase() === giveaway.toLowerCase());
+            const draw = json.draws.find(draw => draw.name.toLowerCase() === giveaway.toLowerCase());
 
-        if(!draw){
-            await interaction.reply({content:'Ingen giveaway med dette navn, prøv tjek /giveaways', ephemeral: true});
-            return;
-        }
+            if(!draw){
+                await interaction.reply({content:'Ingen giveaway med dette navn, prøv tjek /giveaways', ephemeral: true});
+                return;
+            }
 
-        if(draw.winnerChosen) return;
+            if(draw.winnerChosen){
+                await interaction.reply({content:`Vinderen er fundet, tjek ${giveawayChannel} for at se om det var dig!`, ephemeral: true});
+                return;
+            }
 
-        if(draw.participants.some(id => id === interaction.member.id)){
-            await interaction.reply({content: "Du er allerede tilmeldt denne giveaway", ephemeral: true});
-            return;
-        }
+            if(draw.participants.some(id => id === interaction.member.id)){
+                await interaction.reply({content: "Du er allerede tilmeldt denne giveaway", ephemeral: true});
+                return;
+            }
 
-        draw.participants.push(interaction.member.id);
-        fs.writeFileSync(path.resolve(__dirname, '../draws.json'), JSON.stringify(json, null, 4));
+            draw.participants.push(interaction.member.id);
+            fs.writeFileSync(path.resolve(__dirname, '../draws.json'), JSON.stringify(json, null, 4));
 
-        await interaction.reply({content:`du deltager nu i ${draw.name} giveaway!\n Vinderen vil blive annonceret i ${giveawayChannel}`, ephemeral: true});
+            await interaction.reply({content:`du deltager nu i ${draw.name} giveaway!\n Vinderen vil blive annonceret i ${giveawayChannel}`, ephemeral: true});
+        } catch(e){console.error}
     }
 }
--- src/commands/pull.js
+++ src/commands/pull.js
@@ -14,36 +14,50 @@ module.exports = {
                 .setRequired(true)
         }),
     async execute(interaction){
-        const giveaway = interaction.options.getString('giveaway');
-        let winner;
-        let json;
+        try{
+            const giveaway = interaction.options.getString('giveaway');
+            let winner;
+            let json;
 
-        try{json = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../draws.json')));} 
-        catch(e){console.log}
+            try{json = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../draws.json')));} 
+            catch(e){console.log}
 
-        if(interaction.member.roles.highest.comparePositionTo(config.giveaway_role_id) < 0){
-            await interaction.reply({content:'Du har ikke adgang til denne kommando', ephemeral: true})
-            return;
-        }
+            if(interaction.member.roles.highest.comparePositionTo(config.giveaway_role_id) < 0){
+                await interaction.reply({content:'Du har ikke adgang til denne kommando', ephemeral: true})
+                return;
+            }
 
-        const draw = json.draws.find(draw => draw.name.toLowerCase() === giveaway.toLowerCase());
-        if(draw.participants.length === 0){
-            await interaction.reply({content:'Der er ingen deltagere i denne giveaway!', ephemeral: true});
-            return;
-        }
-        if(!draw){
-            await interaction.reply({content:'ingen giveaway med det navn', ephemeral: true});
-            return;
-        }
+            const draw = json.draws.find(draw => draw.name.toLowerCase() === giveaway.toLowerCase());
+            
+            if(draw.winnerChosen){
+                await interaction.reply({content:'Denne giveaway er allerede blevet trukket!', ephemeral: true});
+                return;
+            }
+            if(draw.participants.length === 0){
+                await interaction.reply({content:'Der er ingen deltagere i denne giveaway!', ephemeral: true});
+                return;
+            }
+            if(!draw){
+                await interaction.reply({content:'ingen giveaway med det navn', ephemeral: true});
+                return;
+            }
 
-        const winnerIndex = Math.floor(Math.random() * draw.participants.length);
-        winner = draw.participants[winnerIndex];
+            const winnerIndex = Math.floor(Math.random() * draw.participants.length);
+            winner = draw.participants[winnerIndex];
 
-        const member = await interaction.guild.members.fetch(winner).catch(console.error);
+            const member = await interaction.guild.members.fetch(winner).catch(console.error);
 
-        draw.winnerChosen = true;
-        fs.writeFileSync(path.resolve(__dirname, '../draws.json'), JSON.stringify(json, null, 4));
+            if(member.roles.highest.comparePositionTo(config.rusling_role_id) > 0){
+                draw.participants.splice(winnerIndex, 1);
+                fs.writeFileSync(path.resolve(__dirname, '../draws.json'), JSON.stringify(json, null, 4));
+                await interaction.reply({content:"Ups, en tutor har sneget sig ind i denne giveaway. Prøv igen...", ephemeral: true});
+                return;
+            }
 
-        await interaction.reply({content: `${member} har vundet ${draw.name} giveaway!`})
+            draw.winnerChosen = true;
+            fs.writeFileSync(path.resolve(__dirname, '../draws.json'), JSON.stringify(json, null, 4));
+
+            await interaction.reply({content: `${member} har vundet ${draw.name} giveaway!`});
+        } catch(e){console.error}
     }
 }
